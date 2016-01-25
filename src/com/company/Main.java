30839bc077820a34b34c3147155c275972d26dfc
Tommy Morgan <jangel1@bu.edu>
2016-01-25 15:36:43 -0500

    added more comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -46,6 +46,8 @@ public class Main {
     //can do 3 recursions with no serious delay
 //TODO: gets stuck when users column choice is 7
 
+    //checks every possible combinations of moves from implementation of this method on and adds those that win
+    // to a list of int arrays
     public static List<int[]> RecursiveAi(String[][] blank, int recursions, int[] moves, List<int[]> wins) {
         //cycles through all possible moves for ai and user
         for (int AIchoice = 0; AIchoice < 7; AIchoice++) {
@@ -225,32 +227,52 @@ public class Main {
     //                                     Run!!!!! with betterAIchoice
     //--------------------------------------------------------------------------//
 
-
+//TODO add a break for when the board is full or maybe for when a tie is eminent
     public static void run() throws IOException {
+        //Creates 2dim array that will be board
         String[][] blank = new String[6][7];
+        //calls refresh Method of new board
         blank = refresh(blank);
+        //displays empty board
         display(blank);
+        //loops until someone wins see to do
         while (true) {
+            //saves users column choice input as an int
             int columnChoice = Integer.parseInt(question()) - 1;
+            //calls method checkChoice
             int[] array = checkChoice(columnChoice, blank);
+            //if choice was valid adds marker to game board then calls AI
             if (array[2] == 1) {
+                //adds marker to game board
                 blank[array[1]][columnChoice] = "x";
+                //displays gameboard
                 display(blank);
+                //checks to see if user won
                 if (checkIfWin(blank, "x")) {
+                    //tells user they won
                     System.out.println("YOU WON!!!");
+                    //ends loop and ends program
                     break;
                 }
+                //calls aiChoice to determine AI's next move
                 array = aiChoice(columnChoice,blank);
+                //sets AI's marker to game board
                 blank[array[1]][array[0]] = "o";
-                //blank = aiChoice(columnChoice, blank);
+                //Tells user that AI has made a choice
                 System.out.printf("%nThe AI's Choice!%n");
+                //displays game board with users choice
                 display(blank);
+                //checks to see if AI won
                 if (checkIfWin(blank, "o")) {
+                    //tells user they lost
                     System.out.println("YOU LOST!!!");
+                    //exits loop and ends program
                     break;
                 }
             } else {
+                //tells user to choose another value in case their input was not valid
                 System.out.printf("%nPlease choose again");
+                //goes to next iteration of while loop
                 continue;
             }
         }
@@ -261,42 +283,53 @@ public class Main {
     //--------------------------------------------------------------------------//
 
     private static boolean checkIfWin(String[][] blank, String mark) {
+        //cycles through the rows and columns
         for (int i = 5; i > -0; i--) {
             for (int j = 0; j < 7; j++) {
-
+                //checks to see if each value matches mark which would either be "o" or "x"
                 if (blank[i][j] == mark) {
+                    //int counters for different methods of winning if any of these counters are incremented to 4
+                    // that means that someone won
                     int k = 1;
                     int x = 1;
                     int y = 1;
+                    //only checks diagonally to the right
                     if (j < 4) {
+                        //skips checking verticaly and diagonally  if you are too high up on the board
                         if (i > 2) {
+                            //checks for consecutive "marks" horizontally
                             while (k < 4 && blank[i - k][j] == mark) {
                                 //System.out.printf("%n%s%n",blank[i-k][j] );
                                 k++;
                             }
+                            //checks for consecutive "marks" diagonally with the top to the right
                             while (y < 4 && blank[i - y][j + y] == mark) {
                                 //System.out.printf("%n%s%n",blank[i-y][j+y] );
                                 y++;
                             }
                         }
+                        //checks for consecutive "marks" vertically
                         while (x < 4 && blank[i][j + x] == mark) {
                             //System.out.printf("%n%s  ( %s, %s) %n",blank[i][j+x],i,j+x );
                             x++;
                         }
+                        //checks diagonally with the top to the left and horizontally
                     } else if (j > 3) {
                         if (i > 2) {
+                            //checks vertically
                             while (x < 4 && blank[i - x][j] == mark) {
                                 //System.out.printf("%n%s%n",blank[i-x][j] );
                                 x++;
                             }
+                            //checks diagonally with the top to the left
                             while (y < 4 && blank[i - y][j - y] == mark) {
                                 // System.out.printf("%n%s%n",blank[i-y][j-y] );
                                 y++;
                             }
                         }
                     }
+                    //if any of the counters were incremented to 4 than method return true if not then returns false
                     if (k == 4 || y == 4 || x == 4) {
-                        //System.out.printf("%n %s %s %s %s %s %n", k, y, x, i, j);
                         return true;
                     }
                 }
@@ -310,6 +343,7 @@ public class Main {
     //--------------------------------------------------------------------------//
 
     private static boolean checkC(int columnInt) {
+        //makes sure columnInt is valid integer value
         if (columnInt < 7 && columnInt >= 0) {
             return true;
         }
@@ -321,6 +355,7 @@ public class Main {
     //--------------------------------------------------------------------------//
 
     private static boolean checkR(int rowInt) {
+        //makes sure rowInt is a valid integer value
         if (rowInt < 6 && rowInt >= 0) {
             return true;
         }
@@ -332,14 +367,16 @@ public class Main {
     //--------------------------------------------------------------------------//
 
     public static int whichRow(String[][] screen, int columnInt) {
-
+        //rowInt counter starting from the bottom
         int rowInt = 5;
+        //cycles up the column until the first blank spot is found
         while (screen[rowInt][columnInt] == "x" || screen[rowInt][columnInt] == "o") {
             rowInt--;
             if (rowInt < 0) {
                 break;
             }
         }
+        //the first blank row in the given column is returned as the row choice
         return rowInt;
     }
 
@@ -348,16 +385,23 @@ public class Main {
     //--------------------------------------------------------------------------//
 
     public static int[] checkChoice(int column, String[][] blank) {
+        //new array with three elements is created
         int[] choices = new int[3];
+        //element 0 is the column choice
         choices[0] = column;
+        //element 2 is the validity of the choices with its default value set to not valid ie 0
         choices[2] = 0;
+        //implements checkC method on element 0
         if (checkC(choices[0])) {
+            //if valid column choice the row choice is found using whichRow method
             int temp = whichRow(blank, choices[0]);
+            //if valid row choice then added as element 1 in array and validity is set to true or 1
             if (checkR(temp)) {
                 choices[1] = temp;
                 choices[2] = 1;
             }
         }
+        //returns array
         return choices;
     }
 
@@ -365,21 +409,28 @@ public class Main {
     //          The AI
     //--------------------------------------------------------------------------//
 
+    //very simple AI that chooses the next column based solely on where the user placed his marker
     public static int[] aiChoice(int columnChoice, String[][] blank) {
 
-
+        //loops until valid column choice is found
         while (true) {
+            //creates new array
             int[] array;
+            //creates a new random object
             Random ran = new Random();
-            int rowChoiceAI;
+            //column Choice for ai variable
+            int colChoiceAI;
+            //randomly chooses either the same column or one the surrounding columns
             if (columnChoice == 1) {
-                rowChoiceAI = columnChoice + ran.nextInt(3);
+                colChoiceAI = columnChoice + ran.nextInt(3);
             } else if (columnChoice == 7) {
-                rowChoiceAI = columnChoice - ran.nextInt(3);
+                colChoiceAI = columnChoice - ran.nextInt(3);
             } else {
-                rowChoiceAI = columnChoice + (ran.nextInt(3) - 1);
+                colChoiceAI = columnChoice + (ran.nextInt(3) - 1);
             }
-            array = checkChoice(rowChoiceAI, blank);
+            //makes sure choice is valid and determine row choice
+            array = checkChoice(colChoiceAI, blank);
+            //if choice is valid then array containing validity, row and column choice is returned
             if(array[2] == 1) {
                 return array;
             }
@@ -390,6 +441,11 @@ public class Main {
     //--------------------------------------------------------------------------//
     //          a better Ai choice
     //--------------------------------------------------------------------------//
+
+    //The problem with this is it looks for consecutive "x"'s meaning any area where there are two or three in a row
+    // and this is only defensive programing what it needs
+    // TODO is search for consecutive "o"s as well and become offensive when necessary
+    //TODO only searches for consecutive "marks" will miss if there are 2 in row then blank then 1 or any such combo
 
     //Reformatted the checkIfWin to look at where a win would be possible then add that column to a map
     public static String[][] betterAIChoice(String[][] blank) {
@@ -478,7 +534,7 @@ public class Main {
         }
         //TODO: should be more random but ok for now
         int[] array =  aiChoice(4, blank);
-        //doesn't implement an move so fix that if you think this
+        //doesn't implement a move so fix that if you think this will be usefull
         return blank;
     }
 
@@ -489,17 +545,24 @@ public class Main {
 
 
     public static int bestChoiceCol(int[] locDir, int sizeOfVect) {
+        //determines which column the marker should in go based on the results of the betterAIChoice
         sizeOfVect--;
+        //checks the 2 element in loc dir which determines the direction
         switch (locDir[2]) {
+            //this is diagonally with top to the left
             case 2:
                 return locDir[1] - sizeOfVect;
+            //this is vertically
             case 3:
                 return locDir[1];
+            //this is diagonally with top to the right
             case 4:
                 return locDir[1] + sizeOfVect;
+            //this is horizontally to the right
             case 5:
                 return locDir[1] + sizeOfVect;
         }
+        //returns vertically if all else fails
         return 4;
     }
 
@@ -510,42 +573,70 @@ public class Main {
 
 
     public static void runRecursion() throws IOException {
+        //creates new game board
         String[][] blank = new String[6][7];
+        //fills in blank gameboard
         blank = refresh(blank);
+        //displays blank game board
         display(blank);
+        //creates an int array that will keep track of all of the moves made
         int moves[] = new int[43];
+        //the first element in moves is how many moves have been made
         moves[0] = 0;
+        //creates a list of integer arrays that contain winning moves
         List<int[]> wins = new ArrayList<>();
-
+        //loops until someone wins
         while (true) {
+            //turns user input into an int
             int columnChoice = Integer.parseInt(question()) - 1;
+            //finds validity and row choice
             int[] array = checkChoice(columnChoice, blank);
+            //checks validity
             if (array[2] == 1) {
+                //places users marker on game board
                 blank[array[1]][columnChoice] = "x";
+                //adds column choice to moves array
                 moves[++moves[0]] = columnChoice;
+                //displays game board
                 display(blank);
+                //checks to see if user won
                 if (checkIfWin(blank, "x")) {
+                    //tells user it won
                     System.out.println("YOU WON!!!");
+                    //exits while loop and ends program
                     break;
                 }
+                //calls a simple AI if early on in game and calls Recursive Ai after two turns
                 if(moves[0] < 4) {
+                    //calls recursiveAi method
                     wins = RecursiveAi(blank, 2, moves, wins);
+                    //returns column and choice and validity into int array
                     array = checkWins(wins,blank,moves,columnChoice);
                 }
                 else
                 {
+                    //calls simple AI
                     array = aiChoice(columnChoice,blank);
                 }
+                //adds Ai markers to gameboard
                 blank[array[1]][array[0]] = "o";
+                //adds column choice to moves array
                 moves[++moves[0]] = array[0];
+                //tells user Ai has made its move
                 System.out.printf("%nThe AI's Choice!%n");
+                //displays updated game board
                 display(blank);
+                //checks if AI won
                 if (checkIfWin(blank, "o")) {
+                    //tell user they lost
                     System.out.println("YOU LOST!!!");
+                    //exits loop and ends program
                     break;
                 }
             } else {
+                //tells user to enter a new valid input
                 System.out.printf("%nPlease choose again");
+                //continues to next iteration of while loop
                 continue;
             }
         }
